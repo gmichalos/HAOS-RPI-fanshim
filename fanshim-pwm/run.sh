16cd8dd0c70a742a0f8253bf49a956e6598ac788
gmichalos <32699279+gmichalos@users.noreply.github.com>
2026-01-17 19:27:19 +0200

    Update run.sh

--- fanshim-pwm/run.sh
+++ fanshim-pwm/run.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/with-contenv bashio
-set -e
 
-bashio::log.info "Starting FanSHIM PWM..."
-
-# Wait for GPIO
-while [ ! -e /dev/gpiomem ]; do
-  bashio::log.info "Waiting for /dev/gpiomem..."
-  sleep 1
-done
-
-bashio::log.info "GPIO available"
-
-# Use exec to replace shell with python process
-exec python3 /app/ha_fanshim_pwm.py
+bashio::log.info "Starting FanSHIM PWM service"
+exec python3 /ha_fanshim_pwm.py
