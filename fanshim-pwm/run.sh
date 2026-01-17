277770fe1b46b728df1297b753674173a8f7a30f
gmichalos <32699279+gmichalos@users.noreply.github.com>
2026-01-17 19:29:25 +0200

    Update run.sh

--- fanshim-pwm/run.sh
+++ fanshim-pwm/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Starting FanSHIM PWM service"
-exec python3 /ha_fanshim_pwm.py
+bashio::log.info "========================================="
+bashio::log.info "Starting FanSHIM PWM Add-on"
+bashio::log.info "========================================="
+
+# Run Python script
+exec python3 -u /ha_fanshim_pwm.py
